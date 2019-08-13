153171ad7a06b7534e5f31bdac34b996d8c01b2c
Godwin9911 <agedah99@gmail.com>
2019-08-13 13:17:53 +0100

    branch update godwin

--- express/server.js
+++ express/server.js
@@ -6,12 +6,9 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.json({message: "hello from Json"})
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
